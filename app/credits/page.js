6bc47c9a8e361df59b1e70aadc1d6fd706c9fa19
dedekpo <dedekpo@gmail.com>
2024-03-20 17:37:21 -0300

    first commit

---
+++ app/credits/page.js
@@ -0,0 +1,22 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Court Kings 3D Credits</h1>
+      <p>Created by André Elias @andre_elias98</p>
+      <p>Dedicated to Carolina Rosa Meireles</p>
+      <h2>Songs</h2>
+      <p>C.B.P.D by Arulo</p>
+      <p>Complicated by Arulo</p>
+      <p>Funky Hip Hop by Arulo</p>
+      <p>Trip Hop Vibes by Alejandro Magaña</p>
+      <p>Try Me by Arulo</p>
+      <h2>Models</h2>
+      <p>Court created by Klieg3D and modified by André Elias</p>
+      <p>Ball created by chroma3d</p>
+      <p>City tree by Poly by Google</p>
+      <p>Icons by Icons8</p>
+      <p>Shirts designed by 'Jose Vitor - Design 3D'</p>
+      <p>UI designed by Ryan Franco</p>
+    </div>
+  );
+}
---
+++ app/privacy-policy/page.js
@@ -0,0 +1,92 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Privacy Policy for Court Kings 3D</h1>
+      <p>Effective date: March 20, 2024</p>
+
+      <h2>Welcome to Court Kings 3D</h2>
+      <p>
+        Welcome to Court Kings 3D, a dynamic basketball game available on
+        Poki.com. This privacy policy aims to inform you about how we collect,
+        use, and protect your personal information while you play Court Kings
+        3D, participate in our online multiplayer mode, or interact with our
+        game in any other way.
+      </p>
+
+      <h2>Collection and Use of Information</h2>
+      <h3>Local Storage & Cookies</h3>
+      <p>
+        Court Kings 3D utilizes local storage and cookies to enhance your gaming
+        experience. Local storage is used to save your game progress, settings,
+        and preferences, allowing you to pick up where you left off. Cookies
+        help us understand how you interact with the game, which parts of the
+        game are most popular, and how we can improve your experience. Cookies
+        may also be used to remember your preferences and settings.
+      </p>
+
+      <h3>Multiplayer Mode</h3>
+      <p>
+        Our game offers an online multiplayer mode facilitated by Colyseus, a
+        powerful multiplayer game framework. While engaging in multiplayer
+        sessions, no personal data is stored on our servers. However, temporary
+        data such as your game session information is used to enable real-time
+        gameplay with other players.
+      </p>
+
+      <h3>Advertisements</h3>
+      <p>
+        Court Kings 3D displays ads to provide the game free of charge. These
+        ads may use cookies or similar technologies to provide ads relevant to
+        your interests. The ad networks we partner with are selected for their
+        adherence to privacy standards and regulations.
+      </p>
+
+      <h2>Data Protection</h2>
+      <p>
+        We are committed to protecting your privacy and ensuring the security of
+        your information. Although Court Kings 3D does not store personal data
+        on our servers, we implement robust security measures to protect the
+        data stored on your device through local storage and cookies.
+      </p>
+
+      <h2>Children's Privacy</h2>
+      <p>
+        {`Court Kings 3D complies with the Children's Online Privacy Protection
+        Act (COPPA) and does not knowingly collect personal information from
+        children under the age of 13 without appropriate parental consent. If we
+        learn that we have collected personal information from a child under 13
+        without verification of parental consent, we will take steps to remove
+        that information from our systems.`}
+      </p>
+
+      <h2>Your Rights and Choices</h2>
+      <p>
+        You have the right to access, correct, or delete any personal
+        information that we may have collected and stored. Most web browsers
+        allow you to control cookies through their settings preferences.
+        However, if you choose to disable cookies, it may limit your use of
+        certain features or functions on our game.
+      </p>
+
+      <h2>Changes to Our Privacy Policy</h2>
+      <p>
+        We may update this privacy policy from time to time in response to
+        changing legal, technical, or business developments. When we update our
+        privacy policy, we will take appropriate measures to inform you,
+        consistent with the significance of the changes we make.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions or concerns about this privacy policy or our
+        handling of your personal information, please contact us at
+        contact@courtkings3d.com
+      </p>
+
+      <p>
+        By using Court Kings 3D, you consent to the collection and use of your
+        information as outlined in this privacy policy.
+      </p>
+    </div>
+  );
+}
